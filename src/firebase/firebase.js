246a0acc96935a84781673b981ea5b616242984f
unknown <chris.a.desilva@gmail.com>
2019-02-02 15:50:43 -0600

    Add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,8 +12,9 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default };
+export { firebase, googleAuthProvider, database as default };
 
 // //subscribe for updates when child elements are removed
 // database.ref('expenses').on('child_removed', (snapshot) => {
